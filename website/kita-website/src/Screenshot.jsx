ebbb98182af520dd017e5820fe1b442aab98abb5
Artin karimi <artin962354@proton.me>
2024-06-01 12:26:28 +0330

    Rename kita to persian kita

--- website/kita-website/src/Screenshot.jsx
+++ website/kita-website/src/Screenshot.jsx
@@ -4,7 +4,7 @@ function Screenshot() {
     return (
        <>
             <div>
-                <img src="https://raw.githubusercontent.com/Thecode764/kita/main/screenshots/screenshot.jpeg" width="900" alt="A screenshot from kita"/>
+                <img src="https://raw.githubusercontent.com/Thecode764/kita/main/screenshots/screenshot.jpeg" width="900" alt="A screenshot from persian kita"/>
             </div>
        </>
     )
