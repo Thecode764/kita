9d6a5a1101cc5f1171dd599b1e0fedb3e60b0bca
Artin karimi <artin962354@proton.me>
2024-06-06 15:02:38 +0330

    Add about.py

---
+++ about.py
@@ -0,0 +1,28 @@
+from yachalk import chalk
+
+print(chalk.bold("Welcome to the persian kita project. persian kita is a fork from kita theme with some features added"))
+
+print(chalk.bold("\nFeatures: "))
+
+print(chalk.bg_blue_bright("""
+    Easy to use and modify
+    No preset limits (This theme does not limit your content directory structure, taxonomy names, etc. It's applicable to all zola sites.)
+    Dark mode
+    Responsive design
+    Social icons
+    Taxonomies support
+    Projects page
+    Archive page
+    Table of Content
+    Admonition shortcode
+    SEO friendly
+    Comments using Giscus
+    Mathematical notations using KaTeX
+    Diagrams and charts using Mermaid
+"""))
+
+print(chalk.bg_black("\nGithub:") + chalk.underline.bold.bg_cyan(" https://github.com/Thecode764/kita"))
+
+print(chalk.bg_blue("Website:") + chalk.underline.bold.bg_gray(" https://thecode764.github.io/Kita-website"))
+
+print(chalk.bold("\nFor others visit github"))
