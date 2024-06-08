084c56fed880e1f07bd7d0a415c7362292653c62
Artin karimi <artin962354@proton.me>
2024-06-08 14:41:08 +0330

    🚧 Coverage code

--- mirror/directory-lister/app/resources/js/components/Application.js
+++ mirror/directory-lister/app/resources/js/components/Application.js
@@ -1,18 +1,17 @@
 export default () => ({
     init() {
-        const [cookie, theme] = document.cookie.match(/theme=(dark|light)/) || [null, null];
+        const match = /theme=(dark|light)/.exec(document.cookie);
+        const theme = match? match[1] : null;
 
-        this.$store.theme = theme || (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark');
+        this.$store.theme = theme || (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches? 'light' : 'dark');
 
         if (cookie === undefined) {
             this.storeThemeCookie(this.$store.theme);
         }
     },
-
     toggleTheme() {
-        this.$store.theme = this.$store.theme == 'light' ? 'dark' : 'light';
+        this.$store.theme = this.$store.theme == 'light'? 'dark' : 'light';
     },
-
     storeThemeCookie(theme) {
         document.cookie = `theme=${theme}; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=Lax`;
     }
